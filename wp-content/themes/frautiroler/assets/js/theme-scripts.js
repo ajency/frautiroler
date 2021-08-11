e329efaa39463cbbde9c7e40dc5271f1a5b8a421
Ankita <ankita@ajency.in>
2021-08-11 19:53:56 +0530

    check box fix

--- wp-content/themes/frautiroler/assets/js/theme-scripts.js
+++ wp-content/themes/frautiroler/assets/js/theme-scripts.js
@@ -31,49 +31,6 @@ $('.hamburger-close').on('click', function(){
 });
 
 
-
-
-// jQuery.validator.addMethod("phone_regex", function(value, element) {
-//     return this.optional(element) || /^[0-9\.\-_]{10,30}$/i.test(value);
-// }, "Phone Number with only 0-9. Minlength: 10");
-// $.validator.addMethod('filesize', function (value, element, param) {
-//     return this.optional(element) || (element.files[0].size <= param)
-// }, 'File size must be less than {0}');
-// $("#project-submission").validate({
-//     rules: {
-//         'email': {
-//             required: true,
-//             email: true
-//         },
-//         'phone': {
-//             required: true,
-//             phone_regex: true,
-//             minlength: 10
-//         }
-//     },
-
-//     messages: {
-
-//         'mitgliedsnummer': { required: "Bitte ausfüllen" },
-//         'vorname': { required: "Bitte ausfüllen" },
-//         'nachname': { required: "Bitte ausfüllen" },
-//         'nummer': { required: "Bitte ausfüllen" },
-//         'plz-ort': { required: "Bitte ausfüllen" },
-//         'phone': { required: "Bitte ausfüllen" },
-//         'email': { required: "Bitte ausfüllen" },
-//         'project': { required: "Bitte ausfüllen" },
-//         'detail': { required: "Bitte ausfüllen" },
-//         'projektkosten': { required: "Bitte ausfüllen" },
-//         'myfile': { required: "Bitte mindestens 1 Bild hochladen" },
-//         'legal' : {required: "Bitte ankreuzen"},
-//         'condition' : {required: ""}
-
-//     },
-//     submitHandler: function(form) {
-//         form.submit();
-//     }
-// });
-
 /* heart animation */
 
 $('.click-heart').on('click', function(){
@@ -225,4 +182,16 @@ function checkScreenSize(){
 /* smooth scroll */
 var scroll = new SmoothScroll('a[href*="#"]', {
 	speed: 500
+});
+
+
+
+$("input[type='checkbox']").change(function() {
+    if(this.checked) {
+      $(this).prop('checked', true);
+      $(this).addClass("checked");
+    }else{
+      $(this).prop('checked', false);
+      $(this).removeClass("checked");
+    }
 });
