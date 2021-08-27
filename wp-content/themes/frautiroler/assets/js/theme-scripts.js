e7cb826a64518f93cbc272a292e3a0c9f104a88a
Chaitanyashirodkar <chaitanyashirodkar010@gmail.com>
2021-08-27 13:13:26 +0530

    issue 98 test

--- wp-content/themes/frautiroler/assets/js/theme-scripts.js
+++ wp-content/themes/frautiroler/assets/js/theme-scripts.js
@@ -137,7 +137,7 @@ $(document).ready(function(){
       cnt++;
       if (cnt > 3) cnt = 1;
       ele.addClass("banner-" + cnt);
-    }, 20000);
+    }, 6000);
     var currentDiv = $(".home .site-container-wrapper #logo-1");
     var nextDiv, count = 1;
     var myInterval = setInterval(function() {
